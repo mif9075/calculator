2d87621f674f7c0edeba34765268a0ca11aa2cd1
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-05-20 14:24:28 -0400

    Added Addition, Subtraction, Multiplication and Division old codes

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function init() {
 
      numbersArray = Array.from(document.querySelectorAll('.number'));
 
-     console.log(numbersArray);
+    //  console.log(numbersArray);
     
     numbersArray.forEach(function(element) {element.addEventListener('click', numberKeys);});
 
@@ -42,4 +42,95 @@ function result(event) {
 }
 
 
+//Old Code from Previous Projects
+
+function addToAll(event) {
+    // Make sure page doesn't reload on button press.
+    event.preventDefault();
+
+    // Grab value to add.
+
+    let numberToAdd = document.querySelector('#numberForMath').value;
+
+    if(isNaN(numberToAdd)  || numberToAdd===''){
+        resetInput();
+    }
+    
+    else{
+    // Add value to everything on the list.
+    for(let i = 0; i < numbers.length; i++) {
+        numbers[i] = parseFloat(numbers[i]) + parseFloat(numberToAdd);
+        }
+    }  
+    // Update our html.
+    console.log(numbers);
+    updateUL();
+}
+
+function subtractFromAll(event) {
+    // Make sure page doesn't reload on button press.
+    event.preventDefault();
+    
+    // Grab value to subtract.
+    let numberToSubtract = document.querySelector('#numberForMath').value;
+    
+    if(isNaN(numberToSubtract)  || numberToSubtract===''){
+        resetInput();
+    }
+    
+    else{
+    // Subtract value to everything on the list.
+    for(let i = 0; i < numbers.length; i++) {
+        numbers[i] = parseFloat(numbers[i]) - parseFloat(numberToSubtract);
+        }
+    }
+    // Update our html.
+    console.log(numbers);
+    updateUL();
+}
+
+function multiplyByAll(event) {
+    // Make sure page doesn't reload on button press.
+    event.preventDefault();
+    
+    // Grab value to multiply.
+    let numberToMultiply = document.querySelector('#numberForMath').value;
+
+    if(isNaN(numberToMultiply) || numberToMultiply===''){
+        resetInput();
+    }
+    
+    else{
+        
+    // Grab value to Multiply.
+    for(let i = 0; i < numbers.length; i++) {
+        numbers[i] = parseFloat(numbers[i]) * parseFloat(numberToMultiply);
+    }
+    }
+    // Update our html.
+    console.log(numbers);
+    updateUL();
+}
+
+function divideFromAll(event) {
+    // Make sure page doesn't reload on button press.
+    event.preventDefault();
+
+    // Grab value to divide.
+    let numberToDivide = document.querySelector('#numberForMath').value;
+    if(isNaN(numberToDivide)  || numberToDivide === ''){
+        resetInput();
+    }
+    
+    else{
+
+    // Divide value from everything on the list.
+    for(let i = 0; i < numbers.length; i++) {
+        numbers[i] = parseFloat(numbers[i]) / parseFloat(numberToDivide);
+        }
+    }
+    // Update our html.
+    console.log(numbers);
+    updateUL();
+}
 
