0820c74aa89b3b666c185e723fdf6c2f80a70cee
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-05-20 14:15:48 -0400

    Able to concat numbers together after pressing number key

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
+let numbers = '';
+let numbersArray = [];
+
 window.onload = init;
 
 function init() {
     document.querySelector('.keyOperator')
         .addEventListener('click', operation);
-    document.querySelector('.number')
-        .addEventListener('click', number);
+
+
+     numbersArray = Array.from(document.querySelectorAll('.number'));
+
+     console.log(numbersArray);
+    
+    numbersArray.forEach(function(element) {element.addEventListener('click', numberKeys);});
+
+
     document.querySelector('.clear')
         .addEventListener('click', clear);
     document.querySelector('.keyEqual')
         .addEventListener('click', result);
 }
+
+function operation(event) {
+    event.preventDefault();
+}
+
+function numberKeys (event) {
+    event.preventDefault();
+    let number = event.target.innerText;
+    numbers = numbers.concat(number);
+    // console.log(number);
+    console.log(numbers);
+}
+
+function clear(event) {
+    event.preventDefault();
+}
+
+function result(event) {
+    event.preventDefault();
+}
+
+
+
